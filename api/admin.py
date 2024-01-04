ffcc42478588726f9810f6eebfd3005abcbab997
rakshya98 <rakshyatimalsena@gmail.com>
2024-01-04 16:21:49 +0545

    final commit

---
+++ api/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from  api.models import Item,Location
+# Register your models here.
+admin.site.register(Item)
+admin.site.register(Location)
---
+++ api/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from .views import ItemList,ItemDetail,LocationList,LocationDetail
+from api import views 
+
+urlpatterns = [
+    path('item/',ItemList.as_view()),
+    path('item/<int:pk>/',ItemDetail.as_view()),
+    path('location/',LocationList.as_view()),
+    path('location/<int:pk>/',LocationDetail.as_view()),
+    
+]
